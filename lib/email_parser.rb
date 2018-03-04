3ce5770d30cc2e9798eb736d9558075925d88870
Denise Pendleton <dpendl1@hotmail.com>
2018-03-04 14:58:15 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,8 +4,11 @@
 # or whitespace (' ').
 class EmailParser
   attr_accessor :emails
+
   def initialize(emails)
-      end
+    @emails = emails
+  end
+      
    def parse
       @emails.split(/[ , ]/)
 
