8b46b52ee784d694f06c31917fda5d9e50420219
Denise Pendleton <dpendl1@hotmail.com>
2018-03-04 15:16:33 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,8 @@ class EmailParser
   end
 
    def parse
-      array = @emails.split(", ") || array = @emails.split(/[\s]/)
+       array = @emails.split(/[\s]/)
+      #  array = @emails.split(", ") ||
 
 
    end
